6657277845224687184aa1c8b56b3c48691b9694
ndcroos <nick.decroos@gmail.com>
2018-04-26 11:19:19 -0400

    foo

--- Ray.h
+++ Ray.h
@@ -1,4 +1,12 @@
 class Ray {
 
+public:
+        ray() {}
+        ray(const vec3& a, const vec3& b) { A = a; B = b; }  
+        vec3 origin() const       { return A; }
+        vec3 direction() const    { return B; }
+        vec3 point_at_parameter(float t) const { return A + t*B; }
 
+        vec3 A;
+	vec3 B;
 };
